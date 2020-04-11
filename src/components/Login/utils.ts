07f2e69a8f2011344d516fe56d732909fd45fe06
JaaKto <33008680+JaaKto@users.noreply.github.com>
2020-04-11 23:25:12 +0200

    test commit

--- src/components/Login/utils.ts
+++
@@ -1,9 +0,0 @@
-export const inputList = [
-  { id: 0, type: "email", name: "email", placeholder: "enter your email" },
-  {
-    id: 1,
-    type: "password",
-    name: "password",
-    placeholder: "enter your password",
-  },
-]
